164061eeca7976593dab7f0cfceca174aa9764ad
Chidambar P Bangre <52619562+Chidu0665@users.noreply.github.com>
2022-10-11 19:58:35 +0200

    Delete FINANCE MANAGER directory

--- FINANCE MANAGER/main/adddoc.php
+++
@@ -1,6 +0,0 @@
-<form action="savedoc.php" method="post">
-Transaction Mode<br>
-<input type="text" name="docname" />
-<br>
-<input type="submit" value="Save">
-</form>
--- FINANCE MANAGER/main/addtype.php
+++
@@ -1,6 +0,0 @@
-<form action="saveoffice.php" method="post">
-Category Name<br>
-<input type="text" name="typename" />
-<br>
-<input type="submit" value="Save">
-</form>
